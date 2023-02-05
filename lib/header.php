95bf0652d93e7bbc8e17b5e09dfdf31b08225792
llowq <96781765+kenDevXD@users.noreply.github.com>
2023-02-05 18:09:50 +0700

    Add files via upload

---
+++ lib/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta content="Tools Cek WR Mobile Legends" name="description" />
+      <meta content="Kenn Hiroyuki" name="author" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+      <meta name="keywords" content="cek wr mlbb, cek winrate mobile legends, hitung wr mlbb, hitung winrate mobile legends, mobile legends, tools mobile legends, tools game,tools indonesia, cara menghitung wr, situs penghitung wr, mobile legends, hitung wr, website penghitung wr, penghitung wr, cara hitung wr ml">
+      <meta property="og:description" content="Cek Win Rate Mobile Legends" />
+      <title>Tools Cek WR Mobile Legends</title>
+      
+      <!-- App favicon -->    
+      <link rel="shortcut icon" href="https://i.postimg.cc/SxHMhRw4/blue.png">
+      
+      <!-- General CSS Files -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+           
+      <!-- Template CSS -->
+      <link rel="stylesheet" href="../assets/css/style.css">
+      <link rel="stylesheet" href="../assets/css/components.css">      
+</head>
